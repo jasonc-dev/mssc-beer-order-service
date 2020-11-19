ad3bbd96747e796b6eb1c88446598c3d913d252b
jasonc-dev <jasoncohen07@gmail.com>
2020-11-19 13:31:22 +0000

    improved JMS messaging including Sagas

---
+++ src/main/java/guru/sfg/beer/order/service/services/BeerOrderManager.java
@@ -0,0 +1,19 @@
+package guru.sfg.beer.order.service.services;
+
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.brewery.model.BeerOrderDto;
+
+import java.util.UUID;
+
+public interface BeerOrderManager {
+
+    BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+    void processValidationResult(UUID beerOrderId, Boolean valid);
+
+    void beerOrderAllocationPassed(BeerOrderDto beerOrderDto);
+
+    void beerOrderAllocationPendingInventory(BeerOrderDto beerOrder);
+
+    void beerOrderAllocationFailed(BeerOrderDto beerOrder);
+}
