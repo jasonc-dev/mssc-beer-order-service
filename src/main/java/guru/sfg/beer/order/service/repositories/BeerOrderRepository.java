a918e9362a05e28a2aaf60f92757e157fb8e3dbd
jasonc-dev <jasoncohen07@gmail.com>
2022-01-04 23:00:02 +0000

    Adding Test for happy path integration testing - incremental

--- src/main/java/guru/sfg/beer/order/service/repositories/BeerOrderRepository.java
+++ src/main/java/guru/sfg/beer/order/service/repositories/BeerOrderRepository.java
@@ -23,9 +23,7 @@ import guru.sfg.beer.order.service.domain.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
-import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,12 +31,12 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-01-26.
  */
-public interface BeerOrderRepository  extends JpaRepository<BeerOrder, UUID> {
+public interface BeerOrderRepository extends JpaRepository<BeerOrder, UUID> {
 
     Page<BeerOrder> findAllByCustomer(Customer customer, Pageable pageable);
 
     List<BeerOrder> findAllByOrderStatus(BeerOrderStatusEnum beerOrderStatusEnum);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    BeerOrder findOneById(UUID id);
+//    @Lock(LockModeType.PESSIMISTIC_WRITE)
+//    BeerOrder findOneById(UUID id);
 }
